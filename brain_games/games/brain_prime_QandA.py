0435ef55692b69b74d6206020f5cedce83b0fd13
devriez <a.moiseikin@gmail.com>
2021-01-09 00:10:56 +0300

    9 step

--- brain_games/games/brain_prime_QandA.py
+++ brain_games/games/brain_prime_QandA.py
@@ -1,10 +1,13 @@
 import random
 
+
 def isPrime(a):
     return all(a % i for i in range(2, a))
 
+
 def generate():
-    head_question = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+    head_question = '''Answer "yes" if given number is prime.
+Otherwise answer "no".'''
     number = random.randint(1, 10)
     question = f'{number}'
     answer = 'yes' if isPrime(number) else 'no'
