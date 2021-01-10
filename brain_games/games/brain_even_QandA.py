bc0376ee0831c9d5b404babbd441703457e2bd11
devriez <a.moiseikin@gmail.com>
2021-01-10 11:29:31 +0300

    refactoring brain_even

---
+++ brain_games/games/brain_even_QandA.py
@@ -0,0 +1,12 @@
+import random
+
+def generate_QandA():
+    
+    question = random.randint(0, 100)
+
+    if question % 2 == 0:
+        answer = 'yes'
+    else:
+        answer = 'no'
+
+    return(question, answer)
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
-from brain_games.is_even_logic import is_even
+from brain_games.game_engine import ask_and_check_answer
+from brain_games.games.brain_even_QandA import generate_QandA
 from brain_games.welcome import welcome_user
 
 
 def main():
-    name = welcome_user()
-    is_even(name)
+    user_name = welcome_user()
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    ask_and_check_answer(generate_QandA, user_name)
 
 
 if __name__ == '__main__':
