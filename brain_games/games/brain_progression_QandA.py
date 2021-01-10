52ce4905a0e58f4180a261e53285ff1cebd97d2d
devriez <a.moiseikin@gmail.com>
2021-01-10 23:23:00 +0300

    fix brain_progression

--- brain_games/games/brain_progression_QandA.py
+++ brain_games/games/brain_progression_QandA.py
@@ -3,10 +3,10 @@ import random
 
 def generate_QandA():
     length = random.randint(6, 16)
-    position = random.randint(1, length)
+    position = random.randint(0, length)
     step = random.randint(2, 10)
     start = random.randint(0, 20)
-    finish = start + length * step
+    finish = start + (length + 1) * step
     progression = [str(x) for x in range(start, finish, step)]
     answer = progression[position]
     progression[position] = '..'
