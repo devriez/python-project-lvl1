b1b845e87ecb96fcfc215378749295c51006ddac
devriez <46251030+devriez@users.noreply.github.com>
2021-01-14 19:02:57 +0300

    Update game_engine.py

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -1,9 +1,18 @@
 import prompt
 
+ROUNDS = 3
+
 
 def ask_and_check_answer(game, user_name):
-
-    for i in range(3):
+    '''
+    Ask a question and take user answer.
+    Compare user answer with correct answer.
+    Display for user results
+    :param game: question and right answer
+           user_name: ask user name and display greeting
+    :return None
+    '''
+    for _ in range(ROUNDS):
         (question, correct_answer) = game()
 
         print('Question:', question)
@@ -15,7 +24,5 @@ def ask_and_check_answer(game, user_name):
             print(f'''\'{answer}\' is wrong answer ;(. \
 Correct answer was \'{correct_answer}\'.''')
             print(f"Let's try again, {user_name}!")
-            break
-
-    if i == 2:
-        print(f'Congratulations, {user_name}!')
+            return
+    print(f'Congratulations, {user_name}!')
