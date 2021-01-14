b0597b222af6eda056334b4f508ebc71504699f9
devriez <46251030+devriez@users.noreply.github.com>
2021-01-14 19:04:33 +0300

    Update welcome.py

--- brain_games/welcome.py
+++ brain_games/welcome.py
@@ -2,6 +2,10 @@ import prompt
 
 
 def welcome_user():
+    '''
+    Ask user name and display greetings
+    return: name: user name
+    '''
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print('Hello,', name, '!')
